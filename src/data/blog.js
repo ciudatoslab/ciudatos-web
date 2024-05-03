80ff744961f56ab5062164f9d9bf0e023b664e99
anze28 <pedro2528anze@gmail.com>
2024-05-03 13:05:59 -0400

    function: Independizar el componente del blog del inicio y del sitio del blog

---
+++ src/data/blog.js
@@ -0,0 +1,50 @@
+// articles.json
+export const articles =[
+    {
+      "id": "1",
+      "titulo": "Cómo Bolivia se convirtió en un país urbano y desigual",
+      "fecha": "6/11/2023",
+      "descripcion": "Hoy en día, el 75% de los bolivianos viven en ciudades, por lo cual hacer de ellas un lugar más equitativo debería ser una política prioritaria para las autoridades en todos los niveles de gobierno. ¿Pero realmente se están tomando con seriedad este problema?",
+      "imagen": "/img/tarjetas-articulos/desigualdad-thumbnail.png",
+      "url": "https://ciudatos-bolivia.github.io/urbanizacion-en-bolivia/",
+      "container": "window.location.href='https://ciudatos-bolivia.github.io/urbanizacion-en-bolivia/'"
+    },
+    {
+      "id": "2",
+      "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
+      "fecha": "30/06/2023",
+      "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
+      "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
+      "url": "https://ciudatos-bolivia.github.io/diseno-calles/",
+      "container": "window.location.href='https://ciudatos-bolivia.github.io/diseno-calles/'"
+    },
+    {
+        "id": "3",
+        "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
+        "fecha": "30/06/2023",
+        "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
+        "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
+        "url": "https://ciudatos-bolivia.github.io/diseno-calles/",
+        "container": "window.location.href='https://ciudatos-bolivia.github.io/diseno-calles/'"
+      },
+      {
+          "id": "4",
+          "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
+          "fecha": "30/06/2023",
+          "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
+          "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
+          "url": "https://ciudatos-bolivia.github.io/diseno-calles/",
+          "container": "window.location.href='https://ciudatos-bolivia.github.io/diseno-calles/'"
+        },
+        
+    {
+        "id": "5",
+        "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
+        "fecha": "30/06/2023",
+        "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
+        "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
+        "url": "https://ciudatos-bolivia.github.io/diseno-calles/",
+        "container": "window.location.href='https://ciudatos-bolivia.github.io/diseno-calles/'"
+      }
+  ]
+  
