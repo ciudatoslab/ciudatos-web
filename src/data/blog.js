75080ae00183b469bfda5f5a26ed6d51417cc0d2
valeipr <valeria.peredorodriguez@gmail.com>
2024-05-06 12:59:46 -0400

    fix y content varios: url de cards, contenido de blogs

--- src/data/blog.js
+++ src/data/blog.js
@@ -20,31 +20,31 @@ export const articles =[
     },
     {
         "id": "3",
-        "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
-        "fecha": "30/06/2023",
-        "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
-        "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
-        "url": "https://ciudatos-bolivia.github.io/diseno-calles/",
-        "container": "window.location.href='https://ciudatos-bolivia.github.io/diseno-calles/'"
+        "titulo": "Cantidad de días de lluvia en ciudades capitales de Bolivia el 2021",
+        "fecha": "19/04/2023",
+        "descripcion": "¿Cuántos días del año llovió en las ciduades capitales de bolivia el 2021?",
+        "imagen": "/img/tarjetas-articulos/lluvia.jpeg",
+        "url": "blogs/dias-lluvia",
+        "container": "window.location.href='blogs/dias-lluvia'"
       },
       {
           "id": "4",
-          "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
-          "fecha": "30/06/2023",
-          "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
-          "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
-          "url": "https://ciudatos-bolivia.github.io/diseno-calles/",
-          "container": "window.location.href='https://ciudatos-bolivia.github.io/diseno-calles/'"
+          "titulo": "Brecha salarial en el área urbana de Bolivia",
+          "fecha": "03/05/2023",
+          "descripcion": "Según datos de la encuesta de hogares, en el área urbana de Bolivia existen brechas salariales de género en todas las actividades económicas.",
+          "imagen": "/img/tarjetas-articulos/brecha.jpeg",
+          "url": "blogs/brecha-salarial-urbana",
+          "container": "window.location.href='blogs/brecha-salarial-urbana'"
         },
         
     {
         "id": "5",
-        "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
-        "fecha": "30/06/2023",
-        "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
-        "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
-        "url": "https://ciudatos-bolivia.github.io/diseno-calles/",
-        "container": "window.location.href='https://ciudatos-bolivia.github.io/diseno-calles/'"
+        "titulo": "Mapas 3D de la densidad poblacional en Bolivia",
+        "fecha": "31/05/2023",
+        "descripcion": "🗺 🇧🇴 ¿Dónde viven los bolivianos? Este #MiércolesdeVisualizaciones elaboramos un especial: mapas 3D de la densidad poblacional de los 9 departamentos de Bolivia. ¿Qué ciudades y lugares logras identificar?",
+        "imagen": "/img/tarjetas-articulos/la-paz.jpeg",
+        "url": "blogs/mapas-3d-densidad-poblacional-bolivia",
+        "container": "window.location.href='blogs/mapas-3d-densidad-poblacional-bolivia'"
       }
   ]
   
