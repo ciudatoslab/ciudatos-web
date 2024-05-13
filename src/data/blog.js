d02320f65003ebb45acb17885b56d5007257c6f7
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2024-05-12 21:36:02 -0400

    fix attempt: rutas de blogs

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,119 +1,118 @@
 // articles.json
-export const articles =[
-    {
-      "id": "1",
-      "titulo": "Cómo Bolivia se convirtió en un país urbano y desigual",
-      "fecha": "6/11/2023",
-      "descripcion": "Hoy en día, el 75% de los bolivianos viven en ciudades, por lo cual hacer de ellas un lugar más equitativo debería ser una política prioritaria para las autoridades en todos los niveles de gobierno. ¿Pero realmente se están tomando con seriedad este problema?",
-      "imagen": "/img/tarjetas-articulos/desigualdad-thumbnail.png",
-      "url": "https://ciudatos-bolivia.github.io/urbanizacion-en-bolivia/"
-    },
-    {
-      "id": "2",
-      "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
-      "fecha": "30/06/2023",
-      "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
-      "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
-      "url": "https://ciudatos-bolivia.github.io/diseno-calles/"
-    },
-        
-    {
-        "id": "12",
-        "titulo": "Mapa de museos en Bolivia",
-        "fecha": "18/09/2023",
-        "descripcion": "Bolivia es un país muy rico y diverso en culturas. Además de haber sido el territorio de culturas prehispánicas que dejaron importantes vestigios arqueológicos, es el lugar donde viven varios pueblos indígenas y también tiene una importante historia nacional, y regional que debe recordarse. Uno de los espacios destinados para contar y preservar la memoria y cultura nacional son los museos, ¿pero qué museos hay en Bolivia? ¿Cuántos son y dónde están?",
-        "imagen": "/img/tarjetas-articulos/museos.png",
-        "url": "/mapa-museos-bol"
-      },
+export const articles = [
+  {
+    "id": "1",
+    "titulo": "Cómo Bolivia se convirtió en un país urbano y desigual",
+    "fecha": "6/11/2023",
+    "descripcion": "Hoy en día, el 75% de los bolivianos viven en ciudades, por lo cual hacer de ellas un lugar más equitativo debería ser una política prioritaria para las autoridades en todos los niveles de gobierno. ¿Pero realmente se están tomando con seriedad este problema?",
+    "imagen": "/img/tarjetas-articulos/desigualdad-thumbnail.png",
+    "url": "https://ciudatos-bolivia.github.io/urbanizacion-en-bolivia/"
+  },
+  {
+    "id": "2",
+    "titulo": "Cómo los automóviles se convirtieron en el centro del diseño de las calles bolivianas",
+    "fecha": "30/06/2023",
+    "descripcion": "Las calles bolivianas se están diseñando para servir un tipo específico del transporte: el transporte motorizado. ¿Cómo es que las calles bolivianas terminaron construyéndose únicamente para el automóvil y se olvidaron de los peatones?",
+    "imagen": "/img/tarjetas-articulos/calles-thumbnail2.png",
+    "url": "https://ciudatos-bolivia.github.io/diseno-calles/"
+  },
 
-      {
-        "id": "5",
-        "titulo": "Mapas 3D de la densidad poblacional en Bolivia",
-        "fecha": "31/05/2023",
-        "descripcion": "🗺 🇧🇴 ¿Dónde viven los bolivianos? Este #MiércolesdeVisualizaciones elaboramos un especial: mapas 3D de la densidad poblacional de los 9 departamentos de Bolivia. ¿Qué ciudades y lugares logras identificar?",
-        "imagen": "/img/tarjetas-articulos/la-paz.jpeg",
-        "url": "/mapas-3d-densidad-poblacional-bolivia"
-      },
+  {
+    "id": "12",
+    "titulo": "Mapa de museos en Bolivia",
+    "fecha": "18/09/2023",
+    "descripcion": "Bolivia es un país muy rico y diverso en culturas. Además de haber sido el territorio de culturas prehispánicas que dejaron importantes vestigios arqueológicos, es el lugar donde viven varios pueblos indígenas y también tiene una importante historia nacional, y regional que debe recordarse. Uno de los espacios destinados para contar y preservar la memoria y cultura nacional son los museos, ¿pero qué museos hay en Bolivia? ¿Cuántos son y dónde están?",
+    "imagen": "/img/tarjetas-articulos/museos.png",
+    "url": "/blogs/mapa-museos-bol"
+  },
 
-      {
-        "id": "13",
-        "titulo": "Explorando un laboratorio espacial urbano: funciones híbridas en La Ceja de El Alto",
-        "fecha": "08/02/2023",
-        "descripcion": "La Ceja de El Alto es uno de los núcleos territoriales más importantes área del metropolitana paceña. En este artículo interactivo te contamos cómo se organiza su territorio bajo sus propias lógicas.",
-        "imagen": "/img/tarjetas-articulos/elalto.jpeg",
-        "url": "https://labtecnosocial.org/la-ceja/"
-      },
+  {
+    "id": "5",
+    "titulo": "Mapas 3D de la densidad poblacional en Bolivia",
+    "fecha": "31/05/2023",
+    "descripcion": "🗺 🇧🇴 ¿Dónde viven los bolivianos? Este #MiércolesdeVisualizaciones elaboramos un especial: mapas 3D de la densidad poblacional de los 9 departamentos de Bolivia. ¿Qué ciudades y lugares logras identificar?",
+    "imagen": "/img/tarjetas-articulos/la-paz.jpeg",
+    "url": "/blogs/mapas-3d-densidad-poblacional-bolivia"
+  },
 
-      {
-        "id": "6",
-        "titulo": "Crecimiento de la población urbana en Bolivia",
-        "fecha": "06/11/2023",
-        "descripcion": "Presentamos dos gráficos interactivos que dan cuenta del proceso de urbanización en Bolivia.",
-        "imagen": "/img/tarjetas-articulos/crecimiento-bol.png",
-        "url": "/crecimiento-urbano-bolivia"
-      },
+  {
+    "id": "13",
+    "titulo": "Explorando un laboratorio espacial urbano: funciones híbridas en La Ceja de El Alto",
+    "fecha": "08/02/2023",
+    "descripcion": "La Ceja de El Alto es uno de los núcleos territoriales más importantes área del metropolitana paceña. En este artículo interactivo te contamos cómo se organiza su territorio bajo sus propias lógicas.",
+    "imagen": "/img/tarjetas-articulos/elalto.jpeg",
+    "url": "https://labtecnosocial.org/la-ceja/"
+  },
 
-    {
-        "id": "3",
-        "titulo": "Cantidad de días de lluvia en ciudades capitales de Bolivia el 2021",
-        "fecha": "19/04/2023",
-        "descripcion": "¿Cuántos días del año llovió en las ciudades capitales de bolivia el 2021?",
-        "imagen": "/img/tarjetas-articulos/lluvia.jpeg",
-        "url": "/dias-lluvia"
-      },
-      {
-          "id": "4",
-          "titulo": "Brecha salarial en el área urbana de Bolivia",
-          "fecha": "03/05/2023",
-          "descripcion": "Según datos de la encuesta de hogares, en el área urbana de Bolivia existen brechas salariales de género en todas las actividades económicas.",
-          "imagen": "/img/tarjetas-articulos/brecha.jpeg",
-          "url": "/brecha-salarial-urbana"
-        },
-        
-        {
-            "id": "7",
-            "titulo": "Desigualdad espacial en Cochabamba",
-            "fecha": "06/11/2023",
-            "descripcion": "Mapa interactivo que muestra varios aspectos de desigualdad socioespacial en el municipio de Cochabamba",
-            "imagen": "/img/tarjetas-articulos/desigualdad-cocha.png",
-            "url": "/desigualdad-cochabamba"
-          },
-        
-          {
-              "id": "8",
-              "titulo": "¿A quiénes privilegia el diseño vial?",
-              "fecha": "04/07/2023",
-              "descripcion": "Galería fotográfica que calcula el porcentaje del espacio ocupado por automóviles en las ciudades de La Paz, El Alto, Cochabamba y Tarija.",
-              "imagen": "/img/tarjetas-articulos/galeria-espacio.jpeg",
-              "url": "/espacio-calles"
-            },
-        
-            {
-                "id": "9",
-                "titulo": "Precios del transporte público en países de latinoamérica",
-                "fecha": "04/07/2023",
-                "descripcion": "Comparación de precios del transporte público en diferentes países de latinoamérica.",
-                "imagen": "/img/tarjetas-articulos/costo-transporte.png",
-                "url": "/costo-transporte-publico"
-              },
-        
-              {
-                  "id": "10",
-                  "titulo": "Cantidad de camas hospitalarias por departamento en Bolivia",
-                  "fecha": "02/08/2023",
-                  "descripcion": "¿Cuántas camas hospitalarias hay en cada departamento por mil habitantes?",
-                  "imagen": "/img/tarjetas-articulos/camas-hosp.jpeg",
-                  "url": "/camas-hosp"
-                },
-        
-                {
-                    "id": "11",
-                    "titulo": "Torres de telefonía móvil en Bolivia (2008-2022)",
-                    "fecha": "14/04/2023",
-                    "descripcion": "En este mapa de las antenas de telefonía móvil colocadas en Bolivia entre 2008 y 2022, podemos ver cómo éstas se concentran y conectan los principales centros urbanos del país.",
-                    "imagen": "/img/tarjetas-articulos/torres.png",
-                    "url": "/torres-telefonia-bol"
-                  }
-          
-    ]
-  
+  {
+    "id": "6",
+    "titulo": "Crecimiento de la población urbana en Bolivia",
+    "fecha": "06/11/2023",
+    "descripcion": "Presentamos dos gráficos interactivos que dan cuenta del proceso de urbanización en Bolivia.",
+    "imagen": "/img/tarjetas-articulos/crecimiento-bol.png",
+    "url": "/blogs/crecimiento-urbano-bolivia"
+  },
+
+  {
+    "id": "3",
+    "titulo": "Cantidad de días de lluvia en ciudades capitales de Bolivia el 2021",
+    "fecha": "19/04/2023",
+    "descripcion": "¿Cuántos días del año llovió en las ciudades capitales de bolivia el 2021?",
+    "imagen": "/img/tarjetas-articulos/lluvia.jpeg",
+    "url": "/blogs/dias-lluvia"
+  },
+  {
+    "id": "4",
+    "titulo": "Brecha salarial en el área urbana de Bolivia",
+    "fecha": "03/05/2023",
+    "descripcion": "Según datos de la encuesta de hogares, en el área urbana de Bolivia existen brechas salariales de género en todas las actividades económicas.",
+    "imagen": "/img/tarjetas-articulos/brecha.jpeg",
+    "url": "/blogs/brecha-salarial-urbana"
+  },
+
+  {
+    "id": "7",
+    "titulo": "Desigualdad espacial en Cochabamba",
+    "fecha": "06/11/2023",
+    "descripcion": "Mapa interactivo que muestra varios aspectos de desigualdad socioespacial en el municipio de Cochabamba",
+    "imagen": "/img/tarjetas-articulos/desigualdad-cocha.png",
+    "url": "/blogs/desigualdad-cochabamba"
+  },
+
+  {
+    "id": "8",
+    "titulo": "¿A quiénes privilegia el diseño vial?",
+    "fecha": "04/07/2023",
+    "descripcion": "Galería fotográfica que calcula el porcentaje del espacio ocupado por automóviles en las ciudades de La Paz, El Alto, Cochabamba y Tarija.",
+    "imagen": "/img/tarjetas-articulos/galeria-espacio.jpeg",
+    "url": "/blogs/espacio-calles"
+  },
+
+  {
+    "id": "9",
+    "titulo": "Precios del transporte público en países de latinoamérica",
+    "fecha": "04/07/2023",
+    "descripcion": "Comparación de precios del transporte público en diferentes países de latinoamérica.",
+    "imagen": "/img/tarjetas-articulos/costo-transporte.png",
+    "url": "/blogs/costo-transporte-publico"
+  },
+
+  {
+    "id": "10",
+    "titulo": "Cantidad de camas hospitalarias por departamento en Bolivia",
+    "fecha": "02/08/2023",
+    "descripcion": "¿Cuántas camas hospitalarias hay en cada departamento por mil habitantes?",
+    "imagen": "/img/tarjetas-articulos/camas-hosp.jpeg",
+    "url": "/blogs/camas-hosp"
+  },
+
+  {
+    "id": "11",
+    "titulo": "Torres de telefonía móvil en Bolivia (2008-2022)",
+    "fecha": "14/04/2023",
+    "descripcion": "En este mapa de las antenas de telefonía móvil colocadas en Bolivia entre 2008 y 2022, podemos ver cómo éstas se concentran y conectan los principales centros urbanos del país.",
+    "imagen": "/img/tarjetas-articulos/torres.png",
+    "url": "/blogs/torres-telefonia-bol"
+  }
+
+]
